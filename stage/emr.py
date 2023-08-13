8115fc25a00c6f5eb966d5847fccad3f9204d17a
parandhaman.margan <paran1980@gmail.com>
2023-08-13 15:25:31 +0000

    new addition

--- stage/emr.py
+++ stage/emr.py
@@ -3,6 +3,7 @@ from airflow.decorators import dag
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import BranchPythonOperator
 import pendulum
+from airflow.operators.python import PythonOperator
 from airflow.models.baseoperator import cross_downstream
 from airflow.providers.amazon.aws.operators.emr import EmrCreateJobFlowOperator
 from airflow.providers.amazon.aws.operators.emr import EmrAddStepsOperator
@@ -10,7 +11,11 @@ from airflow.providers.amazon.aws.operators.emr import EmrAddStepsOperator
 from airflow.providers.amazon.aws.sensors.emr import EmrStepSensor
 from airflow.providers.amazon.aws.operators.emr import EmrTerminateJobFlowOperator
 from airflow.utils.trigger_rule import TriggerRule
-
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+import os
+import boto3
+from airflow.hooks.base import BaseHook
+from pathlib import Path
 
 def _check_flag(**context):
     if context['dag_run'].conf['flag']  == "true":
@@ -18,13 +23,35 @@ def _check_flag(**context):
     else:
         return "F1"
 
+#SPARK_STEPS = [
+#    {
+#        'Name': 'hive_task',
+#        'ActionOnFailure': 'CONTINUE',
+#        'HadoopJarStep': { 
+#            'Jar': 'command-runner.jar',              
+#            'Args':['bash', '-c','hive-script --run-hive-script --args -f s3://myawsbucket19080/hiveStep/script/my_taxi_updated.hql -d INPUT=s3://myawsbucket19080/hiveStep/inputData -d OUTPUT=s3://myawsbucket19080/hiveStep/output -d valid_time_stamp=10123456 -d author_name=paran']           
+#    },
+#    },
+#    {
+#      'Name': 'aggregate_task',
+#      'ActionOnFailure': 'CONTINUE',
+#      'HadoopJarStep': {
+#          'Jar': 'command-runner.jar',
+#          'Args': ['spark-submit','s3://myawsbucket19080/scripts/taxi_aggregate.py','s3://myawsbucket19080/hiveStep/output/','s3://myawsbucket19080/hiveStep/agg_output']
+#
+#    },
+#    }
+#]
+
+
+
 SPARK_STEPS = [
     {
         'Name': 'hive_task',
         'ActionOnFailure': 'CONTINUE',
-        'HadoopJarStep': { 
-            'Jar': 'command-runner.jar',              
-            'Args':['bash', '-c','hive-script --run-hive-script --args -f s3://myawsbucket19080/hiveStep/script/my_taxi_updated.hql -d INPUT=s3://myawsbucket19080/hiveStep/inputData -d OUTPUT=s3://myawsbucket19080/hiveStep/output -d valid_time_stamp=10123456 -d author_name=paran']           
+        'HadoopJarStep': {
+            'Jar': 'command-runner.jar',
+            'Args':['bash', '-c','hive-script --run-hive-script --args -f  {{ dag_run.conf["hiveql"] }}   -d INPUT={{ dag_run.conf["input_path"] }} -d OUTPUT={{ dag_run.conf["output_path"] }} -d valid_time_stamp= {{ task_instance.xcom_pull(task_ids="copyFile", key="return_value") }} -d author_name={{ dag_run.conf["author_name"] }}']
     },
     },
     {
@@ -32,61 +59,13 @@ SPARK_STEPS = [
       'ActionOnFailure': 'CONTINUE',
       'HadoopJarStep': {
           'Jar': 'command-runner.jar',
-          'Args': ['spark-submit','s3://myawsbucket19080/scripts/taxi_aggregate.py','s3://myawsbucket19080/hiveStep/output/','s3://myawsbucket19080/hiveStep/agg_output']
+          'Args': ['spark-submit','{{ dag_run.conf["aggregate_script"] }}','{{ dag_run.conf["output_path"] }}','{{ dag_run.conf["aggregate_output"] }}']
 
     },
     }
 ]
 
 
-JOB_FLOW_OVERRIDES = {
-    "Name": "Movie review classifier",
-    "ReleaseLabel": "emr-6.12.0",
-    "Applications": [{"Name": "Hadoop"}, {"Name": "Spark"},{"Name": "Hive"}], # We want our EMR cluster to have HDFS and Spark
-    "Configurations": [
-        {
-            "Classification": "spark-env",
-            "Configurations": [
-                {
-                    "Classification": "export",
-                    "Properties": {"PYSPARK_PYTHON": "/usr/bin/python3"} # by default EMR uses py2, change it to py3
-                
-                },
-                {
-                   "Classification": "spark-hive-site",
-                   "Properties": {
-                   "hive.metastore.client.factory.class": "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"
-                                }
-                }
-
-            ],
-        }
-    ],
-    "Instances": {
-        "InstanceGroups": [
-            {
-                "Name": "Master node",
-                "Market": "SPOT",
-                "InstanceRole": "MASTER",
-                "InstanceType": "m4.xlarge",
-                "InstanceCount": 1,
-            },
-            {
-                "Name": "Core - 2",
-                "Market": "SPOT", # Spot instances are a "use as available" instances
-                "InstanceRole": "CORE",
-                "InstanceType": "m4.xlarge",
-                "InstanceCount": 2,
-            },
-        ],
-        "Ec2KeyName": "emr_key_pair",
-        "KeepJobFlowAliveWhenNoSteps": True,
-        "TerminationProtected": False, # this lets us programmatically terminate the cluster
-        "Ec2SubnetId": "subnet-0f8738060149dc7ca"
-    },
-    "JobFlowRole": "EMR_EC2_DefaultRole",
-    "ServiceRole": "EMR_DefaultRole",
-}
 
 @dag(
     schedule_interval="@hourly",
@@ -94,11 +73,115 @@ JOB_FLOW_OVERRIDES = {
     catchup=False,
 )
 def emrPipeline():
+
+    def getAwsLogin():
+        conn = BaseHook.get_connection('aws_connection')
+        return str(conn.login)
+
+    def getAwsPassword():
+        conn = BaseHook.get_connection('aws_connection')
+        return conn.password
+
+    def validateFile(**kwargs):
+        print("Reading the file")
+        exitFlag=True
+        conn = BaseHook.get_connection('aws_connection')
+        #print(conn)
+        #print(dir(conn))
+        #print(conn.EXTRA_KEY)
+        #print(conn.extra)
+        print(conn._extra)
+        os.environ['AWS_ACCESS_KEY_ID']= conn.login
+        os.environ['AWS_SECRET_ACCESS_KEY']= conn.password
+        os.environ['AWS_REGION']= kwargs['region_name']
+    
+        s3= boto3.client("s3")
+        obj = s3.get_object(Bucket=kwargs['s3_bucketName'] ,Key= kwargs['s3_location'] )
+        lines=obj['Body'].read().decode("utf-8")
+        for line in lines.split("\n"):
+            print(line)
+            if len(line.split(",")) > 4:
+                exitFlag=False
+                break
+        return exitFlag
+
+    bash_env ={
+            "AWS_ACCESS_KEY_ID"  : getAwsLogin(),
+            "AWS_SECRET_ACCESS_KEY" :  getAwsPassword()
+            }
+
+    s3_rename_file_cmd =r""" 
+        . ~/.profile
+        . ~/.bashrc
+        export PATH=$HOME/.local/bin:$PATH
+        s3_buckName=${s3_bucketName}
+        s3_location=${s3_location}
+
+        s3_file_name_without_ext=${s3_location%.*}
+        s3_file_ext=${s3_location: -4}
+
+        """ \
+        + """
+        export AWS_ACCESS_KEY_ID="%s"
+        export AWS_SECRET_ACCESS_KEY="%s"
+        timestamp={{ ts_nodash }}
+        /home/airflow/.local/bin/aws  s3 mv  s3://${s3_buckName}/${s3_location}   s3://${s3_buckName}/${s3_file_name_without_ext}_${timestamp}_validated${s3_file_ext} 
+
+    """%(bash_env.get("AWS_ACCESS_KEY_ID"),bash_env.get("AWS_SECRET_ACCESS_KEY"))
+
+    s3_cp_file_cmd="""
+    s3_buckName=${s3_bucketName}
+    s3_source_dir=${s3_source_dir}
+    s3_target_dir=${s3_target_dir}
+    export AWS_ACCESS_KEY_ID="%s"
+    export AWS_SECRET_ACCESS_KEY="%s"
+    fileName=$(/home/airflow/.local/bin/aws s3 ls s3://${s3_buckName}/${s3_source_dir}/ | sed -n 2p | awk '{ print $4 }')
+    timeStamp=$(echo $fileName | awk -F "_"  '{ print $2}')
+    /home/airflow/.local/bin/aws s3 cp   s3://${s3_buckName}/${s3_source_dir}/$fileName   s3://${s3_buckName}/${s3_target_dir}/
+    echo $timeStamp
+    """%(bash_env.get("AWS_ACCESS_KEY_ID"),bash_env.get("AWS_SECRET_ACCESS_KEY"))
+
     S0 = BashOperator(
         bash_command="sleep 10",
         task_id="S0",
     
     )
+
+    s3_file_check = S3KeySensor(
+    task_id = "s3_file_check",
+    poke_interval=10,
+    timeout=300,
+    soft_fail=False,
+    retries =30,
+    bucket_key="{{ dag_run.conf['s3_location'] }}",
+    bucket_name="{{ dag_run.conf['s3_bucketName'] }}",
+    aws_conn_id="aws_connection"
+    )
+
+    
+    validateFile = PythonOperator(
+    task_id='validate_file',
+    python_callable=validateFile,
+    op_kwargs={'s3_bucketName': "{{ dag_run.conf['s3_bucketName'] }}" , 's3_location': "{{ dag_run.conf['s3_location'] }}", "region_name" : "{{ dag_run.conf['region_name'] }}" },
+    )
+
+    renameFile = BashOperator(
+     task_id='renameFile',
+     bash_command=s3_rename_file_cmd,
+     env = {"s3_bucketName" : "{{ dag_run.conf['s3_bucketName'] }}" ,
+            "s3_location" : "{{ dag_run.conf['s3_location'] }}" }
+     )
+
+    copyFile = BashOperator(
+        task_id="copyFile",
+    bash_command=s3_cp_file_cmd,
+    env= {"s3_bucketName" : "{{ dag_run.conf['s3_bucketName'] }}" ,
+          "s3_source_dir": "{{ dag_run.conf['s3_source_dir'] }}",
+          "s3_target_dir": "{{ dag_run.conf['s3_target_dir'] }}"}
+    )
+
+
+
 
     create_emr_cluster = EmrCreateJobFlowOperator(
      task_id="create_emr_cluster",
@@ -144,7 +227,7 @@ def emrPipeline():
 
     )
 
-    S0 >> create_emr_cluster  >> step_adder >> [step_checker_hive_task , step_checker_agg_task ]>> cluster_remover
-    
+    #S0 >> create_emr_cluster  >> step_adder >> [step_checker_hive_task , step_checker_agg_task ]>> cluster_remover
+    S0 >> s3_file_check >> validateFile >> renameFile >> copyFile >> create_emr_cluster  >> step_adder >> [step_checker_hive_task , step_checker_agg_task ]>> cluster_remover 
 
 dagemr=emrPipeline()
--- stage/moveCsvFile.sh
+++ stage/moveCsvFile.sh
@@ -1,13 +1,10 @@
-s3_buckName="myawsbucket19080"
-s3_source_dir="raw"
-s3_target_dir="validated"
-export AWS_ACCESS_KEY_ID=AKIAYJC6EOICLIH36STV
-export AWS_SECRET_ACCESS_KEY=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
-#aws s3 mv  s3://${s3_buckName}/${s3_location}   s3://${s3_buckName}/${s3_file_name_without_ext}_${timestamp}_validated${s3_file_ext}
+s3_buckName=${s3_bucketName}
+s3_source_dir=${s3_source_dir}
+s3_target_dir=${s3_target_dir}
+export AWS_ACCESS_KEY_ID=${access_key_id}
+export AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
 fileName=$( aws s3 ls s3://${s3_buckName}/${s3_source_dir}/ | sed -n 2p | awk '{ print $4 }')
 timeStamp=$(echo $fileName | awk -F "_"  '{ print $2}')
-echo $fileName
-echo $timeStamp
 aws s3 cp   s3://${s3_buckName}/${s3_source_dir}/$fileName   s3://${s3_buckName}/${s3_target_dir}/
-echo {{ ti.xcom_push(key="timestamp", value=${timeStamp}) }}
+echo $timeStamp
 
