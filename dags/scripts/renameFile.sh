3efe8321dfb18cd86b8ead5b8d4c8cd3e7d96aeb
paran1980 <43312377+paran1980@users.noreply.github.com>
2023-08-14 12:32:38 +0530

    Update renameFile.sh

--- dags/scripts/renameFile.sh
+++ dags/scripts/renameFile.sh
@@ -9,8 +9,8 @@
         s3_file_ext=${s3_location: -4}
 
         
-        export AWS_ACCESS_KEY_ID="AKIAYJC6EOICLQ475Y7D"
-        export AWS_SECRET_ACCESS_KEY="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"
+        export AWS_ACCESS_KEY_ID="XXXXXXXXX"
+        export AWS_SECRET_ACCESS_KEY="XXXXXXXXX"
         timestamp={{ ts_nodash }}
         aws  s3 mv  s3://${s3_buckName}/${s3_location}   s3://${s3_buckName}/${s3_file_name_without_ext}_${timestamp}_validated${s3_file_ext} 
 
