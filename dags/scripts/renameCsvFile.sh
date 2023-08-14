3967c6731c405cc410491f74b7722495af6db499
paran1980 <43312377+paran1980@users.noreply.github.com>
2023-08-14 12:28:48 +0530

    Update renameCsvFile.sh

--- dags/scripts/renameCsvFile.sh
+++ dags/scripts/renameCsvFile.sh
@@ -2,7 +2,7 @@ s3_buckName="myawsbucket19080"
 s3_location="raw/movies.csv"
 s3_file_name_without_ext=${s3_location%.*}
 s3_file_ext=${s3_location: -4}
-export AWS_ACCESS_KEY_ID=AKIAYJC6EOICLIH36STV
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=xxxxxxx
+export AWS_SECRET_ACCESS_KEY=xxxxxx
 timestamp={{ ts_nodash }}
 aws s3 mv  s3://${s3_buckName}/${s3_location}   s3://${s3_buckName}/${s3_file_name_without_ext}_${timestamp}_validated${s3_file_ext}
